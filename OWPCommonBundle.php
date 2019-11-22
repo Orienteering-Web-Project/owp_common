5ff838509aceb752fc52033a7116b2e22707dd28
Quentin GUILLOU <quentin.guillou@atos.net>
2019-11-22 15:47:56 +0100

    Configuration of bundle

--- OWPCommonBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace OWP\OWPCommonBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OWPCommonBundle extends Bundle
-{
-
-}
---
+++ OWPCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace OWP\OWPCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OWPCoreBundle extends Bundle
+{
+
+}
