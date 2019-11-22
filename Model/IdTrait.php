630b61c32aaa728338ac6b86c9dd742a2500ce3a
Quentin GUILLOU <quentin.guillou@atos.net>
2019-11-22 15:22:16 +0100

    Creating OWP Core Models

---
+++ Model/IdTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OWP\OWPCommonBundle\Model;
+
+Trait IdTrait
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
