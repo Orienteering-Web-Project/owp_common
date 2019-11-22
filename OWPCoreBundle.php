9240e1a3589c3ab7196179b390b8853cf4b8eb00
qguillou <guillou.quentin.29@gmail.com>
2019-11-22 21:53:03 +0100

    Dependency injection for Owp Core Bundle

--- OWPCoreBundle.php
+++ OWPCoreBundle.php
@@ -1,9 +1,15 @@
 <?php
+
 namespace Owp\OwpCore;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OwpCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+    }
 }
