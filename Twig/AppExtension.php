18a4355e10a48c4b4bd5dbb28e0b7523dfbda4c9
Quentin GUILLOU <quentin.guillou@atos.net>
2019-11-29 10:36:24 +0100

    Correction de l'affichage des dates (périodes)

--- Twig/AppExtension.php
+++ Twig/AppExtension.php
@@ -21,9 +21,9 @@ class AppExtension extends AbstractExtension
         if (empty($end) || $start->getTimestamp() === $end->getTimestamp()) {
             $date = strftime('%A %d %B %Y à %H:%M', $start->getTimestamp());
         } elseif ($start->format('d') === $end->format('d')) {
-            $date = strftime('%A %d %B %Y de %H:%M', $start->getTimestamp()) . ' - ' . strftime(' à %H:%M', $start->getTimestamp());
+            $date = strftime('%A %d %B %Y de %H:%M', $start->getTimestamp()) . strftime(' à %H:%M', $start->getTimestamp());
         } else {
-            $date = strftime('\D\u %A %d %B %Y à %H:%M \a\u ', $start->getTimestamp()) . ' - ' . strftime('%A %d %B %Y à %H:%M', $end->getTimestamp());
+            $date = strftime('%A %d %B %Y à %H:%M ', $start->getTimestamp()) . ' au ' . strftime('%A %d %B %Y à %H:%M', $end->getTimestamp());
         }
 
         return $date;
